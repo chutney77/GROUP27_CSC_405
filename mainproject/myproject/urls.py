806a93ac5656a258de9af355b2b52029da35c96a
chutney77 <wahabseyitan01@gmail.com>
2026-02-07 14:39:52 -0800

    Update urls.py

--- mainproject/myproject/urls.py
+++ mainproject/myproject/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Uniguide/', include('uniguide.urls', namespace='uniguide')),  # our app URLs
+    path("admin/", admin.site.urls),
+    path("", include("firstpage.urls")),
 ]
