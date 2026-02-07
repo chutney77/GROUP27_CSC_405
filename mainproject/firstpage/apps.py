38752aee75d9a2e0257e436675df06f6cd688052
chutney77 <wahabseyitan01@gmail.com>
2026-02-07 09:51:01 -0800

    Update apps.py

--- mainproject/firstpage/apps.py
+++ mainproject/firstpage/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FirstpageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'firstpage'
+    name = 'mainproject.firstpage'
