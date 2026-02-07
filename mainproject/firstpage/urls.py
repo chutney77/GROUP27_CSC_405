5175daae816fba60a46df90f5a80033ed0356521
chutney77 <wahabseyitan01@gmail.com>
2026-02-07 13:58:36 -0800

    Update urls.py

--- mainproject/firstpage/urls.py
+++ mainproject/firstpage/urls.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+app_name = 'uniguide'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Uniguide/', include('firstpage.urls', namespace='uniguide')),  # your app namespace
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('logout/', views.logout_view, name='logout'),
 ]
-
