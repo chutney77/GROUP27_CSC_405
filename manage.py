5795f6a2cb16473c5e844b37ac7164c36f41b932
chutney77 <wahabseyitan01@gmail.com>
2026-02-07 09:35:57 -0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,12 +2,18 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainproject.settings')
+def main():
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'mainproject.myproject.settings'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django..."
+            "Couldn't import Django."
         ) from exc
     execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
