9e105bc03f13536bcd0fff21bf4838a2e9e1edce
chutney77 <wahabseyitan01@gmail.com>
2026-02-07 14:53:05 +0100

    Remove old myproject directory and keep mainproject

--- myproject/firstpage/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Accounts
-
-# Register your models here.
-from django.contrib import admin
-from .models import Accounts
-
-admin.site.register(Accounts)
