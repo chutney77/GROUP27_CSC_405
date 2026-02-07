9b98781a88ba04e5e334a27152c2286a452566c3
chutney77 <wahabseyitan01@gmail.com>
2026-02-07 08:18:44 -0800

    Update urls.py

--- mainproject/myproject/urls.py
+++ mainproject/myproject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('firstpage.urls')),  # your firstpage app will handle '/'
-    path('admin/', admin.site.urls),      # admin stays at '/admin/'
+    path('', include('firstpage.urls')),  # root points to your firstpage app
+    path('admin/', admin.site.urls),
 ]
